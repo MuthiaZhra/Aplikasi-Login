00dde015cbd9325ff87ca5a7351b0e42ac4f3ac5
Muthiazr <128697651+MuthiaZhra@users.noreply.github.com>
2023-07-30 14:52:20 +0700

    Update login-script.js
    Menambahkan komentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -9,17 +9,24 @@ const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+//memyimpan informasi email dan password
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+//memberi aksi kepada tombol
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  //memberikan nilai kepada setiap inputan email dan password
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+  
+  //mengecek email dan password
   if (email == expectedEmail && password == expectedPassword) {
+    //jika benar akan diarahkan ke halaman utama/berikutnya
     goToHome();
   } else {
+    //jika salah akan memunculkan pop up
     showPopUp();
   }
 });
